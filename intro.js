02d05a6f441ea277d5e57a9315e12598c8c7faed
kumar-pixel <kumarravishankar1999@gmail.com>
2021-06-11 22:07:18 +0530

    successful

---
+++ intro.js
@@ -0,0 +1,11 @@
+console.log("Ravi Shankar")
+let a;
+console.log(a);
+a = 10;
+a = 10.5
+console.log(a);
+a = "Hey,This is my first class";
+a = "Hi, This is my first class too";
+console.log(a);
+
+
